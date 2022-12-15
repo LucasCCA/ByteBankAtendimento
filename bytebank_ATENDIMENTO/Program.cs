0276a672b3a6032d43722a851287eab9f5ed7c5a
Lucas <lucasamarale@hotmail.com>
2022-12-14 23:11:57 -0300

    Aula 01 - Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,109 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
+//TestaArrayInt();
+//TestaBuscarPalavra();
 
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do array {idades.Length}");
+
+    int acumulador = 0;
+
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.WriteLine($"Digite {i + 1}ª palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+    }
+
+}
+
+//Array amostra = Array.CreateInstance(typeof(double), 5);
+//amostra.SetValue(5.9, 0);
+//amostra.SetValue(1.8, 1);
+//amostra.SetValue(7.1, 2);
+//amostra.SetValue(10, 3);
+//amostra.SetValue(6.9, 4);
+//TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"A mediana é igual a {mediana}");
+}
+
+//int[] valores = { 10, 58, 36, 47 };
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine(valores[i]);
+//}
+
+double[] array = new double[3];
+array[0] = 5;
+array[1] = 10;
+array[2] = 6;
+
+Console.WriteLine(CalculaMedia(array));
+double CalculaMedia(double[] array)
+{
+    double acumulador = 0;
+
+    if ((array.Length == 0) || (array == null))
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        acumulador += array[i];
+    }
+
+    double media = acumulador / array.Length;
+    return media;
+}
